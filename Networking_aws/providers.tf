2f19b5ee6009a4c16224d890a0be045048c5cc34
Divyasree Nayani <divyasree_nayani@epam.com>
2022-08-20 13:43:14 +0530

    vpc

---
+++ Networking_aws/providers.tf
@@ -0,0 +1,12 @@
+terraform {
+    required_providers {
+        aws = {
+            source = "hashicorp/aws"
+        }
+    }
+}
+
+# configure the AWS provider
+provider "aws" {
+    region = var.aws_region
+}
